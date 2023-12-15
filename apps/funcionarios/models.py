b238def60f307fcb2196f5a7e2ce87aa4d2a88fb
Julio Salvino <79772860+juliosalvino@users.noreply.github.com>
2023-12-14 22:24:17 -0300

    feat - contrução dos models e relacionamento entre eles

---
+++ apps/funcionarios/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+from apps.departamentos.models import Departamento
+from apps.empresa.models import Empresa
+
+# Create your models here.
+
+
+class Funcionario(models.Model):
+    nome_funcionario = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
+    lista_departamentos = models.ManyToManyField(Departamento)
+    empresa = models.ForeignKey(Empresa, on_delete=models.PROTECT)
+
+    
+    def __str__(self):
+        return self.nome_funcionario
